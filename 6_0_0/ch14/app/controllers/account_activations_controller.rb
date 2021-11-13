92d14578fd6cb59244418ac4c1dd5b872312d849
gccj <garycruise.cj@gmail.com>
2021-11-13 14:19:58 +0900

    Notifications are now sent when a user logs in for the first time.

--- 6_0_0/ch14/app/controllers/account_activations_controller.rb
+++ 6_0_0/ch14/app/controllers/account_activations_controller.rb
@@ -3,11 +3,17 @@ class AccountActivationsController < ApplicationController
   def edit
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
-      user.update_attribute(:activated,    true)
-      user.update_attribute(:activated_at, Time.zone.now)
+      # Memo: ビジネスロジックはctrlに置くべきではない。(かつ、User#activateにすでに対応済み)
+      # user.update_attribute(:activated,    true)
+      # user.update_attribute(:activated_at, Time.zone.now)
       user.activate
       log_in user
-      flash[:success] = "Account activated!"
+      # 初回ログインはこの一回かつ要確認な物もないので、下記で十分と思います。
+      flash[:success] = "初回ログインありがとうございます。"
+      # 意味合い的に一緒なので、もしどうしても「Account activated!」を表示したい場合、
+      # 下記のようにflash msgを追加する形で良いと思います。
+      # flash[:success] = "Account activated!"
+      # flash[:notice] = "初回ログインありがとうございます。"
       redirect_to user
     else
       flash[:danger] = "Invalid activation link"
